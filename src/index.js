12809819f751caff83f20e09cc9328a1b6d65a9e
Familia Hernandez Carbajal <marcela.hdez.1801@gmail.com>
2019-02-01 18:06:21 -0600

    Cipher.js realice la formula para cifrar y descifrar y las puse dentro de mi objeto. index.html realice cambios en los nombres de los id y quite todos los onclicks. index.js modifique las funciones de cambio de pantallas y ya hice la funcion que me imprime la respuesta y me llama a mi objeto

--- src/index.js
+++ src/index.js
@@ -1,52 +1,105 @@
+//Variables donde se guardan los valores para cifrar, descifrar y los desplazamientos
+const strTwo = document.getElementById("safeWordecode").value;//Trae el valor a descifrar
+const offsetTwo = parseInt(document.getElementById("offsetTwo").value);//Trae el valor de offset para descifrar
+const strAgain = document.getElementById("decifradoSafeWord").value;//Trae el valor del password cifrado
+const offsetNew = parseInt(document.getElementById("nuevoOffset").value);//Trae el valor de offset para descifrar
+
+//Cambio de pantallas
 let idPantallaActual = "root";
-document.getElementById("screenGenerarPassword").style.display ="none";
-document.getElementById("screenCambiarPasswordActual").style.display ="none";
-document.getElementById("screenChangePassword").style.display ="none";
+document.getElementById("pantallaGenerarPassword").style.display ="none";
+document.getElementById("pantallaPasswordActual").style.display ="none";
+document.getElementById("pantallaPasswordActualDescifrado").style.display ="none";
 document.getElementById("yourPassword").style.display ="none";
 document.getElementById(idPantallaActual).style.display="block";
 
-screenGenerarPassword= () => {
+//Cambio de pantallas
+const GenerarPassword = () => {
     document.getElementById("root").style.display ="none";
+    document.getElementById("pantallaGenerarPassword").style.display ="block";
+    document.getElementById("pantallaPasswordActual").style.display ="none";
+    document.getElementById("pantallaPasswordActualDescifrado").style.display ="none";
     document.getElementById("yourPassword").style.display ="none";
-    document.getElementById("screenGenerarPassword").style.display ="block";
-}
+};
+document.getElementById("GenerarPassword").addEventListener("click",GenerarPassword);
 
-screenCambiarPasswordActual= () => {
+//Cambio de pantallas
+const CambiarPasswordActual= () => {
     document.getElementById("root").style.display ="none";
-    document.getElementById("screenGenerarPassword").style.display ="none";
+    document.getElementById("pantallaGenerarPassword").style.display ="none";
+    document.getElementById("pantallaPasswordActual").style.display ="block";
+    document.getElementById("pantallaPasswordActualDescifrado").style.display ="none";
     document.getElementById("yourPassword").style.display ="none";
-    document.getElementById("screenCambiarPasswordActual").style.display ="block";
-}
+};
+document.getElementById("CambiarPasswordActual").addEventListener("click",CambiarPasswordActual);
 
-generarPasswordNuevo= () => {
-    document.getElementById("screenChangePassword").style.display ="block";
+//Cambio de pantallas
+const passwordNuevo= () => {
     document.getElementById("root").style.display ="none";
-    document.getElementById("screenGenerarPassword").style.display ="none";
-    document.getElementById("screenCambiarPasswordActual").style.display ="none";
-    document.getElementById("yourPassword").style.display ="none";
-}
-
-PasswordNuevo= () => {
-    document.getElementById("screenChangePassword").style.display ="none";
-    document.getElementById("root").style.display ="none";
-    document.getElementById("screenGenerarPassword").style.display ="none";
-    document.getElementById("screenCambiarPasswordActual").style.display ="none";
+    document.getElementById("pantallaGenerarPassword").style.display ="none";
+    document.getElementById("pantallaPasswordActual").style.display ="none";
+    document.getElementById("pantallaPasswordActualDescifrado").style.display ="none";
     document.getElementById("yourPassword").style.display ="block";
-}
+};
+document.getElementById("passwordNuevo").addEventListener("click",passwordNuevo);
 
-changePassword= () => {
-    document.getElementById("screenChangePassword").style.display ="none";
+
+//Cambio de pantallas
+const generaPasswordNuevo= () => {
     document.getElementById("root").style.display ="none";
-    document.getElementById("screenGenerarPassword").style.display ="none";
-    document.getElementById("screenCambiarPasswordActual").style.display ="none";
-    document.getElementById("yourPassword").style.display ="block";
-}
+    document.getElementById("pantallaGenerarPassword").style.display ="none";
+    document.getElementById("pantallaPasswordActual").style.display ="none";
+    document.getElementById("pantallaPasswordActualDescifrado").style.display ="block";
+    document.getElementById("yourPassword").style.display ="none";
+};
+document.getElementById("generaPasswordNuevo").addEventListener("click",generaPasswordNuevo);
 
-star= () => {
-    document.getElementById("screenChangePassword").style.display ="none";
+//Cambio de pantallas
+const changePassword= () => {
+    document.getElementById("root").style.display ="none";
+    document.getElementById("pantallaGenerarPassword").style.display ="none";
+    document.getElementById("pantallaPasswordActual").style.display ="none";
+    document.getElementById("pantallaPasswordActualDescifrado").style.display ="none";
+    document.getElementById("yourPassword").style.display ="block";
+};
+document.getElementById("changePassword").addEventListener("click",changePassword);
+
+
+//Cambio de pantallas
+const star= () => {
     document.getElementById("root").style.display ="block";
-    document.getElementById("screenGenerarPassword").style.display ="none";
-    document.getElementById("screenCambiarPasswordActual").style.display ="none";
-    document.getElementById("yourPassword").style.display ="none"; 
-}
+    document.getElementById("pantallaGenerarPassword").style.display ="none";
+    document.getElementById("pantallaPasswordActual").style.display ="none";
+    document.getElementById("pantallaPasswordActualDescifrado").style.display ="none";
+    document.getElementById("yourPassword").style.display ="none";
+    document.getElementById("safeWord").value= " "; 
+    document.getElementById("safeWordecode").value= " ";
+    document.getElementById("offset").value= " ";
+    document.getElementById("offsetTwo").value= " ";
+    document.getElementById("decifradoSafeWord").value= " ";
+    document.getElementById("nuevoOffset").value= " ";
+};
+document.getElementById("star").addEventListener("click",star);
+
+
+const cipherWord = () => {
+    const str = document.getElementById("safeWord").value;//Trae el valor a cifrar
+    const offset = parseInt(document.getElementById("offset").value);//Trae el valor de offset cifar
+    let cipherOne = window.cipher.encode(str,offset);
+    document.getElementById("yourSafeWord").innerHTML = cipherOne;
+    return cipherOne;
+ };
+document.getElementById("passwordNuevo").addEventListener("click",cipherWord);
+
+/*document.getElementById("safeWordecode").innerHTML.addEventListener("click",cipher.decode(strTwo,offsetTwo));
+document.getElementById("yourSafeWord").innerHTML.addEventListener("click",cipher.encodeAgain(strAgain,offsetNew));*/
+
+/*   
+const bottonNewPassword = () => {document.getElementById("PasswordNuevo")
+bottonNewPassword.addEventListener("click",encode())}
+const bottonDechiperPassword = () => {document.getElementById("generaPasswordNuevor")
+bottonNewPassword.addEventListener("click",decode())}
+const bottonPasswordForOther = () => {document.getElementById("changePassword")
+bottonNewPassword.addEventListener("click",encode())}
+*/
+
 
